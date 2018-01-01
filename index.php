68c150c71a270182f8c7099bccb8ac3a3377f152
Erivan Cledson <erivancled@hotmail.com>
2018-01-01 14:13:37 -0200

    criando graficos em php com chart.min

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+$vendas = array(10,20,30,40,20);
+$custos = array(8,15,37,97,35);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto Gráficos</title>
+</head>
+<body>
+
+	<div style="width:500px">
+		<canvas id="grafico"></canvas>
+	</div>
+	
+	<script type="text/javascript" src="Chart.min.js"></script>
+	<script type="text/javascript">
+	//assim que a pagina é carregada ja mostra o grafico
+		window.onload = function(){
+			var contexto = document.getElementById("grafico").getContext("2d");
+			//inicia a biblioteca
+			var grafico = new Chart(contexto, {
+				//grafico do tipo linha
+				type:'line',
+				data: {
+					labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+					datasets: [{
+						label:'Vendas',
+						backgroundColor:'#FF0000',
+						borderColor:'#FF0000',
+						data:[<?php echo implode(',', $vendas); ?>],
+						fill:false
+					}, {
+						label:'Custos',
+						backgroundColor:'#00FF00',
+						borderColor:'#00FF00',
+						data:[<?php echo implode(',', $custos); ?>],
+						fill:false
+					}]
+				}
+			});
+		}		
+	</script>
+</body>
+</html>
